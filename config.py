82201ae810b2c6aea54021e650eeb25168565b96
кιяαи ѕєя <105064751+Kiranxer@users.noreply.github.com>
2026-09-10 00:33:51 +0530

    Update FORCE_SUB_CHANNEL environment variable

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ DB_NAME = os.environ.get("DB_NAME", "FileStoreNeon")
 
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1004472965009"))
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003899319204"))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002704790611"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1004436006537"))
 
 FILE_AUTO_DELETE = int(os.environ.get("FILE_AUTO_DELETE", "600"))  # auto delete in seconds
 PORT = os.environ.get("PORT", "8080")
